4453a65bca9454aa3d59d4163acb80d21c542fdf
#_Juan_Zuñiga_Jara# <163361221+cabritox@users.noreply.github.com>
2024-04-04 16:26:16 -0300

    camara.py

--- camara.py
+++ camara.py
@@ -1,8 +1,11 @@
+#Creacion de la clase objeto 'Camara'
 class Camara:
     def __init__(self,queso,maduracion):
+        #Definiendo atributos privados
         self.__queso = queso
         self.__maduracion = maduracion
-        
+
+    #Metodo de simulacion de 'camara de maduracion'
     def produccion(self):
         print(f"En proceso de maduracion: Quesos {self.__queso} ")
         print(f"Condiciones de camara: {self.__maduracion}")
