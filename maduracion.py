e7a9668ab31af4b27b9db6bb1372165431cf2a5e
#_Juan_Zuñiga_Jara# <163361221+cabritox@users.noreply.github.com>
2024-04-04 16:29:07 -0300

    maduracion.py

--- maduracion.py
+++ maduracion.py
@@ -1,15 +1,16 @@
-"""
-
-"""
+#Creacion de la clase maduracion
 class Maduracion:
+    #Definiendo atributos mediante constructor __init__
     def __init__(self,queso,humedad,temperatura):
         self.__queso = queso
         self.__humedad = humedad
         self.__temperatura = temperatura
-        
+
+    #Metodo accesador al atributo 'queso'
     def queso(self):
         print(self.__queso)
-        
+
+    #Metodo que establece las condiciones necesarias para la conservacion del producto
     def elaboracion(self,queso):
         if queso == "Blandos" or queso == "Cremoso":
             if 10<= self.__temperatura <= 14 and 85 <= self.__humedad <= 90 :
